6a2d474456693b3e26151bbdb2bc31004e92b4d1
Mamun1646 <mimbd0113000gmail.com>
2023-01-01 22:35:34 +0600

    environment variable setup

--- database/connection.tsx
+++ database/connection.tsx
@@ -1,18 +1,23 @@
 import { Sequelize } from "sequelize";
 
-const Connection = new Sequelize('mamun', 'root', "", {
-    host: 'localhost',
-    dialect:'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-  });
-  
+const database = process.env.DB_NAME;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSRORD;
+const host = process.env.DB_HOST;
 
-  const testConnection=async()=>{
-    try {
-      await Connection.authenticate();
-      console.log('Connection has been established successfully.');
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-}}
+const Connection = new Sequelize(database, username, password, {
+  host,
+  dialect:"mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+});
+
+const testConnection = async () => {
+  try {
+    await Connection.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 testConnection();
 
 export default Connection;
